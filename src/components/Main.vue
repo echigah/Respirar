54004787283dfcaeac8e8d533cedf41b44d4cdc0
echigah <capurissedgard@gmail.com>
2022-08-09 20:29:39 -0300

    better bootstrap import - globally

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -21,9 +21,8 @@
         </form>
       </div>
     </nav>
-    <router-view/>   
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> 
+    <router-view/>
+        
   </div>
 </template>
 
